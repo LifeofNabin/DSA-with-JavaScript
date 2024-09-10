17bf7efd14fe302a1107674c81e96c16451bf867
Nabin <70052993+LifeofNabin@users.noreply.github.com>
2024-09-11 03:16:02 +0900

    Update reversing_array.js

--- Problems/reversing_array.js
+++ Problems/reversing_array.js
@@ -1,1 +1,21 @@
 Code:
+function Revarr(arr) {
+  let start = 0;
+  let end = arr.length - 1;
+
+
+  while (start < end) {
+    let temp = arr[start];
+    arr[start] = arr[end];
+    arr[end] = temp;
+
+   
+    start++;
+    end--;
+  }
+
+  
+  console.log(arr);
+}
+Revarr([10, 20, 40, 31]);
+
