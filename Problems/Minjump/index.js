88a7b4ae6cc96572c1f76ea51496efcc9c0049ca
Nabin <dhamala.nabin1@gmail.com>
2024-10-03 23:05:13 +0900

    add

--- Problems/Minjump/index.js
+++ Problems/Minjump/index.js
@@ -3,4 +3,24 @@ function minJumps(arr) {
   if (n <= 1) return 0; // No jumps needed for empty or single element array
   if (arr[0] === 0) return Infinity; // Cannot move anywhere if first element is 0
 
-  
+  let jumps = 0;
+  let currentEnd = 0;
+  let farthest = 0;
+
+  for (let i = 0; i < n - 1; i++) {
+    farthest = Math.max(farthest, i + arr[i]); // Update the farthest point
+
+    // If we have come to the end of the current jump
+    if (i === currentEnd) {
+      jumps++; // We need to make a jump
+      currentEnd = farthest; // Update the end for the next jump
+
+      // If we've reached or surpassed the last index, we can stop
+      if (currentEnd >= n - 1) break;
+    }
+  }
+
+  return jumps;
+}
+
+
